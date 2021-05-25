2169b1be808a40429027e04f34d75107d862a656
Sharaar <walidsharar@gmail.com>
2021-05-25 15:22:07 +0000

    Challenge Number was done

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+
 #first Python program 
 print("Hello World!")
+
+
+''' 
+challenge no 1
+print the exact
+Day 1 - Python Print Function
+*/'''
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
 
