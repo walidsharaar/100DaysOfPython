6cb3bab1127d31f12d99ddb416532a8d025e5a96
Sharaar <walidsharar@gmail.com>
2021-05-25 15:44:43 +0000

    Input function usage covered

--- main.py
+++ main.py
@@ -25,14 +25,12 @@ print("Hello to new world"+" "+"Stranger")
 '''challenge no2
 Debug the code 
 
-
 print(Day 1 - String Manipulation")
 print("String Concatenation is done with the "+" sign.")
   print('e.g. print("Hello " + "world")')
 print(("New lines can be created with a backslash and n.")
 
-
-that when you run your program, it should print the following:
+When you run your program, it should print the following:
 
 Day 1 - String Manipulation
 String Concatenation is done with the "+" sign.
@@ -40,8 +38,14 @@ e.g. print("Hello " + "world")
 New lines can be created with a backslash and n.
 '''
 
-
 print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
+print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+
+#Input function
+
+input("What is your name: ")
+
+
