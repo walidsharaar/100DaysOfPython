5e9f63aa10b958dce91f700931f70d556d6261a8
Sharaar <walidsharar@gmail.com>
2021-05-25 15:26:57 +0000

    String Manipulation

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
+#String manipulation
+
+print("Hello World!\nHello World again!")
