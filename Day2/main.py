443cfc070c09c539b51f884cb52c183c1eb79ff0
Sharaar <walidsharar@gmail.com>
2021-05-26 14:00:00 +0200

    Type conversion error handling

---
+++ Day2/main.py
@@ -0,0 +1,21 @@
+#Data Types
+
+#String
+print("Hello"[4])
+#Integer
+print(123+234)
+#float
+print(3.13159)
+#boolean
+True
+False
+
+
+num_char = len(input("Enter your name?"))
+print(num_char)
+# check the type of variable
+print(type(num_char))
+
+print("Your name has " + num_char +"characters.")
+
+
