b33e5cbcc207372d38eb38ada5fc993a2a9322cb
Sharaar <walidsharar@gmail.com>
2021-05-26 14:18:16 +0200

    Challenge No 1 completed

--- Day2/main.py
+++ Day2/main.py
@@ -14,8 +14,36 @@ False
 num_char = len(input("Enter your name?"))
 print(num_char)
 # check the type of variable
+
 print(type(num_char))
 
-print("Your name has " + num_char +"characters.")
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char +" characters.")
+
+
+'''
+Challenge Number 1 of day 2
+Instructions
+Write a program that adds the digits in a 2 digit number. 
+e.g. if the input was 35, then the output should be 3 + 5 = 8
+Warning. Do not change the code on lines 1-3. 
+Your program should work for different inputs. 
+e.g. any two-digit number.
+Hint
+Try to find out the data type of two_digit_number.
+Think about what you learnt about subscripting.
+Think about type conversion.
+
+'''
+
+input_digit = input("Enter a two digit number please:\n")
+first_digit= int(input_digit[0])
+second_digit=int(input_digit[1])
+print(first_digit+second_digit)
+
+
+
+
 
 
