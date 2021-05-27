64f63c813cfda8c6cae0bf15a73dbd4ea0db05a5
Sharaar <walidsharar@gmail.com>
2021-05-27 13:57:59 +0200

    Challenge number one of day three

---
+++ Day3/challenge1_BMI-version2.py
@@ -0,0 +1,29 @@
+'''
+Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+
+It should tell them the interpretation of their BMI based on the BMI value.
+
+Under 18.5 they are underweight
+Over 18.5 but below 25 they have a normal weight
+Over 25 but below 30 they are slightly overweight
+Over 30 but below 35 they are obese
+Above 35 they are clinically obese.
+
+'''
+
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+
+bmi =round(weight/(height*height))
+
+if bmi <= 18.5 :
+    print(f"Your bmi is: {bmi}, you are underweight.")
+elif bmi <= 22:
+    print(f"Your bmi is: {bmi}, you have a normal weight.")
+elif bmi <= 28:
+    print(f"Your bmi is: {bmi}, you slightly overweight.")
+elif bmi <= 30:
+    print(f"Your bmi is: {bmi}, you are obese")
+
+else:
+    print(f"Your bmi is: {bmi}, you are clinically obese")
