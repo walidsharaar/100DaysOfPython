e023bd7a23538306f5017907c1e4da62f230d074
Sharaar <walidsharar@gmail.com>
2021-05-27 13:02:41 +0200

    Conditional Statement reviewed

--- Day3/main.py
+++ Day3/main.py
@@ -1,7 +1,17 @@
 #if/else statments
 
+#tab example
 water_level=50
 if water_level>80:
     print("Drain water")
 else:
     print("Continue")
+
+#rollercoaster
+print("Welcome stranger!!!")
+height=int(input("What is your height in cm?\n"))
+if height >= 120:
+    print("Enjoy your Ride")
+
+else:
+    print("You are not eligible! Sorry")
