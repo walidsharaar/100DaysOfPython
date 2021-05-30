ecc71ff7a98272b68966f56c26f46610be4dc966
Sharaar <walidsharar@gmail.com>
2021-05-30 15:04:23 +0200

    Function declaration

---
+++ Day6/main.py
@@ -0,0 +1,11 @@
+
+# how to create a function
+
+def my_function():
+    print("This is a simple function")
+    print("Hope you like the functionality")
+
+# how to call the function
+my_function()
+
+
