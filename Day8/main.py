73bfa4bbbb7612db5f799f62d4e199371b774cdd
Sharaar <walidsharar@gmail.com>
2021-06-01 13:55:54 +0200

    Keyword Arguments

--- Day8/main.py
+++ Day8/main.py
@@ -1,12 +1,15 @@
 # input functions in python
 
-# input functions in python
-
-def my_fucntion(name):
+def my_function(name,location):
 
     print(f"Greeting form a function {name} !")
     print(f"How do you do {name}? ")
-    print(f"{name} isn't the weather nice today?")
+    print(f"{name} isn't the weather nice today in {location}?")
+    
+my_function(input("Enter your name:\n"),"")
 
 
-my_fucntion(input("Enter your name:\n"))
+
+# using of keyword arguments
+
+my_function(name=input("Enter your name:"),location=input("Enter the location:"))
