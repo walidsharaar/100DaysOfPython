13c3d56475d34f4c57f55d7299f01ed05c3b8d1c
Sharaar <walidsharar@gmail.com>
2021-06-03 17:51:04 +0200

    Practice of day 10

--- Day10/main.py
+++ Day10/main.py
@@ -1,7 +1,15 @@
 # Functions with outputs
 
 
-def my_function():
-    result = 3*2
-    return
+def my_function(f_name,l_name):
+    print(f_name.title())
+    print(l_name.title())
+#formated
+    format_f_name =f_name.title()
+    format_l_name=l_name.title()
+    print(f"{format_l_name}, {format_f_name}")
 
+my_function("walid","sharaar")
+
+
+#
