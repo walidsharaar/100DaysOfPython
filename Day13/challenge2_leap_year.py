0ad0a2bef93996865a75d72824963a5bf740e51d
Sharaar <walidsharar@gmail.com>
2021-06-11 12:37:11 +0200

    challenge2 leap year code debugging completed

--- Day13/challenge2_leap_year.py
+++ Day13/challenge2_leap_year.py
@@ -6,7 +6,7 @@ No shortcuts - don't copy-paste to replace the code entirely with a working solu
 Fix the code so that it works and when you hit submit it should pass all the tests.
 '''
 
-year = input("Which year do you want to check?")
+year = int(input("Which year do you want to check?"))
 
 if year % 4 == 0:
     if year % 100 == 0:
