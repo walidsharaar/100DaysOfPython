316c538eb222a2fbe900b2f17171521033273886
Sharaar <walidsharar@gmail.com>
2021-06-16 11:24:04 +0200

    Day 16 practice

---
+++ Day16_OOP/main.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+
+#construct object
+
+turtle=Turtle()
+print(turtle)
+turtle.shape("turtle")
+turtle.color("DarkOrange")
+turtle.forward(100)
+
+screen = Screen()
+print(screen.canvheight)
+screen.exitonclick()
