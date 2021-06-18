325b09c18d0d380037098668d5e81ddd28c17b1f
Sharaar <walidsharar@gmail.com>
2021-06-18 16:47:39 +0200

    Day 18 practice challenge 2 dash line

--- Day18_GUI/main.py
+++ Day18_GUI/main.py
@@ -5,8 +5,14 @@ my_turtle.shape("turtle")
 my_turtle.turtlesize(3)
 my_turtle.color("red")
 
-
 for _ in range(4):
 
     my_turtle.forward(100)
     my_turtle.right(90)
+
+# dash line
+for _ in range(15):
+    my_turtle.forward(10)
+    my_turtle.penup()
+    my_turtle.forward(10)
+    my_turtle.pendown()
