1fe9a68a4aa4d747508aba5ec88ad1d346503ca2
Sharaar <walidsharar@gmail.com>
2021-06-18 16:41:48 +0200

    Day 18 practice challenge 1 refinement

--- Day18_GUI/main.py
+++ Day18_GUI/main.py
@@ -6,12 +6,7 @@ my_turtle.turtlesize(3)
 my_turtle.color("red")
 
 
+for _ in range(4):
 
-my_turtle.forward(100)
-my_turtle.right(90)
-my_turtle.forward(100)
-my_turtle.right(90)
-my_turtle.forward(100)
-my_turtle.right(90)
-my_turtle.forward(100)
-my_turtle.right(90)
+    my_turtle.forward(100)
+    my_turtle.right(90)
