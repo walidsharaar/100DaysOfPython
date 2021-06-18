2b64bc2df6b336572d4d3a48d59f287b6bc0292e
Sharaar <walidsharar@gmail.com>
2021-06-18 17:05:20 +0200

    Day 18 practice challenge 3 draw different shapes refinement

--- Day18_GUI/main.py
+++ Day18_GUI/main.py
@@ -1,9 +1,11 @@
+import random
 from turtle import Turtle
 
 my_turtle = Turtle()
 my_turtle.shape("turtle")
 my_turtle.turtlesize(3)
 my_turtle.color("red")
+
 
 for _ in range(4):
 
@@ -18,10 +20,15 @@ for _ in range(15):
     my_turtle.pendown()
 
 # create different shapes
-
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 slide_num = 5
+def draw_shape(slide_num):
+    angle = 360 / slide_num
+    for _ in range(slide_num):
+        my_turtle.forward(200)
+        my_turtle.right(angle)
 
-for _ in range(slide_num):
-    angle = 360/slide_num
-    my_turtle.backward(200)
-    my_turtle.right(angle)
+# generate the shapes
+for shape_side_no in range(3,11):
+    my_turtle.color(random.choice(colours))
+    draw_shape(shape_side_no)
