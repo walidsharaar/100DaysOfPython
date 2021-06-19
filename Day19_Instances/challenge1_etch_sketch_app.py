883fe5ced288501010cd86973d2dd5e0e1b60331
Sharaar <walidsharar@gmail.com>
2021-06-19 12:11:49 +0200

    Final refinement

--- Day19_Instances/challenge1_etch_sketch_app.py
+++ Day19_Instances/challenge1_etch_sketch_app.py
@@ -1,37 +1,34 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
 
-my_turtle = Turtle()
+tim = Turtle()
 screen = Screen()
 
 
-def move_on():
-    my_turtle.forward(10)
+def move_forwards():
+    tim.forward(10)
 
-def move_back():
-    my_turtle.backward(10)
+def move_backwards():
+    tim.backward(10)
 
-def move_right():
-    new_heading= my_turtle.heading()-10
-    my_turtle.heading(new_heading)
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
 
-def move_left():
-    new_heading= my_turtle.heading()+10
-    my_turtle.heading(new_heading)
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
 
 def clear():
-    my_turtle.clear()
-    my_turtle.penup()
-    my_turtle.home()
-    my_turtle.pendown()
-
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 screen.listen()
-screen.onkey(move_on,"Up")
-screen.onkey(move_back,"Down")
-screen.onkey(move_right,"Right")
-screen.onkey(move_left,"Left")
-screen.onkey(clear,"c")
-
-
+screen.onkey(move_forwards, "Up")
+screen.onkey(move_backwards, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+screen.onkey(clear, "c")
 
 screen.exitonclick()
