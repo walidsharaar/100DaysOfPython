e07428941d00cf592313e7a9051e2300ae0c9792
Sharaar <walidsharar@gmail.com>
2021-06-19 12:01:24 +0200

    code refinement

--- Day19_Instances/challenge1_etch_sketch_app.py
+++ Day19_Instances/challenge1_etch_sketch_app.py
@@ -18,3 +18,20 @@ def move_left():
     new_heading= my_turtle.heading()+10
     my_turtle.heading(new_heading)
 
+def clear():
+    my_turtle.clear()
+    my_turtle.penup()
+    my_turtle.home()
+    my_turtle.pendown()
+
+
+screen.listen()
+screen.onkey(move_on,"Up")
+screen.onkey(move_back,"Down")
+screen.onkey(move_right,"Right")
+screen.onkey(move_left,"Left")
+screen.onkey(clear,"c")
+
+
+
+screen.exitonclick()
