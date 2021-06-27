640663bea57265cf4d629d8aaf44278e009d8709
Sharaar <walidsharar@gmail.com>
2021-06-27 14:50:28 +0200

    pick one tab of CSV

--- Day25_readingCSVwithPandas/main.py
+++ Day25_readingCSVwithPandas/main.py
@@ -1,8 +1,10 @@
 import csv
-weather_list = []
 
-with open("weather_data.csv") as weather:
-    weathers= csv.reader(weather)
-    for row in weathers:
-        print(row)
+with open("weather_data.csv") as data_file:
+    data= csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+print(temperatures)
 
