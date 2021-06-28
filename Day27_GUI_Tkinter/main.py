a055f601ac36cc4b2738e4167e6b9d5eb4254a42
Sharaar <walidsharar@gmail.com>
2021-06-28 13:13:23 +0200

    Arg * practices

--- Day27_GUI_Tkinter/main.py
+++ Day27_GUI_Tkinter/main.py
@@ -9,7 +9,13 @@ window.minsize(width=500,height=300)
 my_label= tkinter.Label(text="I am label",font=("Arial",24,"bold"))
 my_label.pack()
 
+def add(*args):
+    sum=0
+    for n in args:
+        sum +=n
+    return  sum
 
+print(add(121,323,4123,123,1231,11))
 
 
 
