f337204ed9dfc439eb5c6300acab1838daa04960
Sharaar <walidsharar@gmail.com>
2021-06-28 10:11:12 +0200

    list comprehension

--- Day26_List_Dictionary_Comprehensions/main.py
+++ Day26_List_Dictionary_Comprehensions/main.py
@@ -1,8 +1,16 @@
 #list comprehension
+# new_list = [new_item for item in list]
 
+#normal list
 numb= [1,2,3]
 new_list =[]
 for n in numb:
     add_1 = n+1
     new_list.append(add_1)
+print(new_list)
+
+# list comprehension
+
+name="Khwaja Walid Sharaar"
+new_list = [letter for letter in name]
 print(new_list)
