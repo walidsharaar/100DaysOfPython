b7c218b08d8c036de3f60320cc65e4ffbd33ce23
Sharaar <walidsharar@gmail.com>
2021-06-28 10:46:38 +0200

    Challenge 3 of day 26 is completed

--- Day26_List_Dictionary_Comprehensions/challenge3_data_overlap.py
+++ Day26_List_Dictionary_Comprehensions/challenge3_data_overlap.py
@@ -32,6 +32,13 @@ Example Output
 [3, 6, 5, 33, 12, 7, 42, 13]
 '''
 
+with open("file1.txt")as file1:
+    file_1_data = file1.readlines()
+
+with open("file2.txt") as file2:
+    file_2_data = file2.readlines()
+
+result = [int(num) for num in file_1_data if num in file_2_data]
 
 
 
