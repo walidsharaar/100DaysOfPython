2091c5fa07851bcee1c52f2d47d8052f42841006
Sharaar <walidsharar@gmail.com>
2021-06-28 12:15:57 +0200

    Challenge 4 of day 26 completed

--- Day26_List_Dictionary_Comprehensions/challenge4_dictionary_comprehension.py
+++ Day26_List_Dictionary_Comprehensions/challenge4_dictionary_comprehension.py
@@ -41,8 +41,12 @@ Hint
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 # Don't change code above 👆
 
+result ={word:len(word) for word in sentence.split()}
+
+
+
+
+
 # Write your code below:
-
-
 
 print(result)
