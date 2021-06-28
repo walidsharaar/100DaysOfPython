f2246335da673ba86db060a25d7fcaf4d4935108
Sharaar <walidsharar@gmail.com>
2021-06-28 10:25:40 +0200

    list comprehension with conditional statement

--- Day26_List_Dictionary_Comprehensions/main.py
+++ Day26_List_Dictionary_Comprehensions/main.py
@@ -14,3 +14,16 @@ print(new_list)
 name="Khwaja Walid Sharaar"
 new_list = [letter for letter in name]
 print(new_list)
+
+num_list = [number*2 for number in range(1,5)]
+print(num_list)
+
+# conditional list comprehension
+#new_list = [new_item for item in list if test]
+
+names=["Alex","Beth","Dave","Carolina","Eleanor","Freddie"]
+new_name = [name for name in names if len(name) < 5]
+print(new_name)
+
+capital_name = [name.upper() for name in names if len(name) < 5]
+print(capital_name)
