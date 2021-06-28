f4a6ba4bb50000af180ab4be04549d3089558b7e
Sharaar <walidsharar@gmail.com>
2021-06-28 12:59:47 +0200

    Day 27 exercises

---
+++ Day27_GUI_Tkinter/main.py
@@ -0,0 +1,27 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("GUI In Tkinter")
+window.minsize(width=500,height=300)
+
+#label
+
+my_label= tkinter.Label(text="I am label",font=("Arial",24,"bold"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
