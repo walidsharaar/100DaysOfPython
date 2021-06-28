ee0bc2ed68b2ad75fc8b5bbd308bab8e51ffd14f
Sharaar <walidsharar@gmail.com>
2021-06-28 10:40:49 +0200

    Challenge 3 of day 26

---
+++ Day26_List_Dictionary_Comprehensions/challenge3_data_overlap.py
@@ -0,0 +1,42 @@
+'''
+List Comprehension 3
+
+💪 This exercise is HARD
+Instructions
+
+Take a look inside file1.txt and file2.txt. They each contain a bunch of numbers, each number on a new line.
+
+You are going to create a list called result which contains the numbers that are common in both files.
+
+e.g. if file1.txt contained:
+
+1
+
+2
+
+3
+
+and file2.txt contained:
+
+2
+
+3
+
+4
+
+result = [2, 3]
+
+IMPORTANT: The result should be a list that contains Integers, not Strings. Try to use List Comprehension instead of a Loop.
+Example Output
+
+[3, 6, 5, 33, 12, 7, 42, 13]
+'''
+
+
+
+
+
+
+# Write your code above 👆
+
+print(result)
