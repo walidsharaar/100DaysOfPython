c0b41e76b4a9b7cbf8b5aaaa7a823ab96e8ddb57
Sharaar <walidsharar@gmail.com>
2021-06-29 17:36:06 +0200

    Challenge 1 get Kanye quote completed

--- Day33_API/challenge1_kanye_quote.py
+++ Day33_API/challenge1_kanye_quote.py
@@ -5,6 +5,12 @@ import requests
 
 
 def get_quote():
+    response = requests.get("https://api.kanye.rest/")
+    response.raise_for_status()
+    data=response.json()
+    quote= data["quote"]
+    canvas.itemconfig(quote_text,text=quote)
+
 
 
 
