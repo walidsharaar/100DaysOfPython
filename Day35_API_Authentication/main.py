0ab967e8198cb10318df49e68a77e3c6aa52c965
Sharaar <walidsharar@gmail.com>
2021-06-30 09:09:53 +0200

    check if the weather is raining and warns

--- Day35_API_Authentication/main.py
+++ Day35_API_Authentication/main.py
@@ -8,9 +8,21 @@ weather_params={
     "lat":LAT,
     "lon":LONG,
     "appid":API_KEY,
+    "exclude":"current,minutely,daily"
 }
 
 #current weather
 
 response = requests.get(OWM_ENDPOINT,params=weather_params)
-print(response.json())
+response.raise_for_status()
+weather_data=response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+for hour_data in weather_slice:
+    connection_code= hour_data["weather"][0]["id"]
+    if int(connection_code)<700:
+        will_rain=True
+if will_rain:
+        print("Bring an umbrella.")
+# print(weather_data["hourly"][0]["weather"][0]["id"])
