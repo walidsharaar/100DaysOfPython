8ac5572467e95e83792ea0f43ab8a048b3603406
Sharaar <walidsharar@gmail.com>
2021-06-30 10:04:44 +0200

    SMS to user number refinement

--- Day35_API_Authentication/send_sms.py
+++ Day35_API_Authentication/send_sms.py
@@ -1,5 +1,9 @@
+import os
 import requests
 from twilio.rest import Client
+from twilio.http.http_client import   TwilioHttpClient
+
+
 API_KEY= "94d611536bbb1ddde200017784621c27"
 OWM_ENDPOINT="https://api.openweathermap.org/data/2.5/onecall"
 LAT=52.5244
@@ -31,7 +35,9 @@ for hour_data in weather_slice:
         will_rain=True
 
 if will_rain:
-    client = Client(account_sid, auth_token)
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    client = Client(account_sid, auth_token,http_client=proxy_client)
     message = client.messages \
         .create(
         body="it is raining. Have an umbrella.",
