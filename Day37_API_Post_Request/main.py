1d840efbb6c46f57b902aacb226ee0ecb2b6e637
Sharaar <walidsharar@gmail.com>
2021-07-03 16:03:18 +0200

    add headers to the API

--- Day37_API_Post_Request/main.py
+++ Day37_API_Post_Request/main.py
@@ -1,11 +1,34 @@
 import requests
 PIXELA_ENDPOINT="https://pixe.la/v1/users"
+TOKEN= "ITcanbeANYSecretKey"
+USER="walid"
+
 user_param ={
-    "token":"ITcanbeANYSecretKey",
-    "username":"walid",
+    "token":TOKEN,
+    "username":USER,
     "agreeTermsOfService":"yes",
     "notMinor":"yes",
 }
 
-response=requests.post(url=PIXELA_ENDPOINT,json=user_param)
+# response=requests.post(url=PIXELA_ENDPOINT,json=user_param)
+# print(response.text)
+
+graph_endpoint=f"{PIXELA_ENDPOINT}/{USER}/graphs"
+graph_config={
+    "id":"graph1",
+    "name":"Running",
+    "unit":"km",
+    "type":"float",
+    "color":"ajisai",
+}
+
+# graph_response=requests.post(url=graph_endpoint,json=graph_config)
+# print(graph_response.text)
+
+headers={
+    "X-USER-TOKEN":TOKEN
+}
+
+response=requests.post(url=graph_endpoint,json=graph_config,headers=headers)
 print(response.text)
+
