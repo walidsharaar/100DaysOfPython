61680bf75ec86dbf13524e2d1320edba29d699f4
Sharaar <walidsharar@gmail.com>
2021-07-22 17:35:17 +0200

    got the read article name , link and score

--- Day45_BeautifulSoup/main.py
+++ Day45_BeautifulSoup/main.py
@@ -9,3 +9,9 @@ soup= BeautifulSoup(contents,"html.parser")
 #object then the name of the tag or string of the tag
 
 print(soup.title.string)
+
+# read string of a related tag
+
+all_anchor_tag = soup.find_all(name="a")
+for tag in all_anchor_tag:
+    print(tag.string)
