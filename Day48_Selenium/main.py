4a1404da9aa9d63229ae94840ffdc0bf71e1164a
Sharaar <walidsharar@gmail.com>
2021-07-23 20:36:56 +0200

    file element by Id

--- Day48_Selenium/main.py
+++ Day48_Selenium/main.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
 
 CHROME_DRIVER_PATH= "C:/Webdrivers/chromedriver.exe"
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+driver.get("https://www.amazon.com/Washable-Replaceable-Lupantte-Cognitive-Developmental/dp/B089NPCN4F/ref=sr_1_13_sspa?dchild=1&qid=1627065247&rnid=16225005011&s=baby-products-intl-ship&sr=1-13-spons&psc=1&smid=A173TL47D5PM1H&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExUDA4MUVONE5MMFVaJmVuY3J5cHRlZElkPUEwNTEzMzU2M1RVRkVHVkY4VlI5WCZlbmNyeXB0ZWRBZElkPUEwNDE0MTA5MzBWVEc3NlNBRVJIRyZ3aWRnZXROYW1lPXNwX210Zl9icm93c2UmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl")
+
+price = driver.find_element_by_id("priceblock_ourprice")
+
+print(price.text)
 
 
 
-driver= webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+
+
+
+#driver.close()
+driver.quit()
